68182090446be7bb212bb1a8ee1a19bc0b0ae57c
Théo Mode <milesxbridges@gmail.com>
2022-07-02 16:33:47 -0700

    filter functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,29 +3,31 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import workJSON from 'public/work.json'
-import { unslugify } from 'utils/helpers'
+import { unslugify, slugify } from 'utils/helpers'
+
+interface workProps {
+  type: string
+  title: string
+}
 
 const Home: NextPage = () => {
-  const [work, setWork] = React.useState([])
+  /* consume json   */
+  const [work, setWork] = React.useState<workProps[]>([])
   const _work = React.useMemo(async () => {
     const jsonArray = [workJSON]
 
     return workJSON
-    // return await Promise.all(jsonArray.map(url => fetch(url))).then(async res => {
-    //     return Promise.all(res.map(async data => await data.json()))
-    // })
   }, [])
   React.useMemo(async () => {
     const work = await _work
 
-    // @ts-ignore
     setWork(work)
   }, [_work])
 
   /*  get types */
   const types = React.useMemo(() => {
-    return work.reduce((acc: string[] = [], cv: { type: string }) => {
-      if (!acc.includes(cv.type)) {
+    return work.reduce((acc: string[] = [], cv: workProps) => {
+      if (!acc.includes(unslugify(cv.type))) {
         acc.push(unslugify(cv.type))
       }
 
@@ -33,23 +35,39 @@ const Home: NextPage = () => {
     }, [])
   }, [work])
 
-  const workByType = React.useMemo(() => {
-    if(!types) return
+  /*  prepare work filter */
+  const [filter, setFilter] = React.useState<string>('')
+  const workByType = React.useMemo<any | undefined>(() => {
+    if (!types) return
 
-    console.log('work', work)
+    return types.reduce((acc: [] = [], cv: string) => {
+      acc = { ...acc, [cv]: work.filter((item) => item.type === slugify(cv)) }
 
+      return acc
+    }, [])
   }, [types])
 
   return (
-    <div className="p-12 bg-slate-100 min-h-screen">
-      <div className={'mt-24 flex-col'}>
+    <div className="flex p-12 pt-48 bg-slate-100 min-h-screen">
+      <div className={'flex-col min-w-[290px] items-center justify-center'}>
         {types &&
           types.map((type) => (
-            <button key={type} className={'flex text-3xl hover:text-rose-500 p-4'}>
+            <button
+              key={type}
+              className={'flex text-3xl hover:text-rose-500 p-4'}
+              onClick={() => setFilter(type)}
+            >
               {type}
             </button>
           ))}
       </div>
+      {workByType && (
+        <div className="w-full">
+          {workByType[filter]?.map((item: workProps) => (
+            <div>{item.title}</div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -10,3 +10,26 @@ export const unslugify = (slug: string) => {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
     })
 }
+
+
+
+/**
+ *
+ * Slugify
+ *
+ */
+
+export const slugify = (string: string) => {
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+    const p = new RegExp(a.split('').join('|'), 'g')
+
+    return string.toString().toLowerCase()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+        .replace(/&/g, '-and-') // Replace & with 'and'
+        .replace(/[^\w-]+/g, '') // Remove all non-word characters
+        .replace(/--+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, '') // Trim - from end of text
+}

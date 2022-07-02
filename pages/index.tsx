48b49cff7f595c579568938d8004756e616910f5
Théo Mode <milesxbridges@gmail.com>
2022-07-02 14:27:59 -0700

    init data and layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,51 @@
+import React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import workJSON from 'public/work.json'
+import { unslugify } from 'utils/helpers'
 
 const Home: NextPage = () => {
+  const [work, setWork] = React.useState([])
+  const _work = React.useMemo(async () => {
+    const jsonArray = [workJSON]
+
+    return workJSON
+    // return await Promise.all(jsonArray.map(url => fetch(url))).then(async res => {
+    //     return Promise.all(res.map(async data => await data.json()))
+    // })
+  }, [])
+  React.useMemo(async () => {
+    const work = await _work
+
+    // @ts-ignore
+    setWork(work)
+  }, [_work])
+
+  /*  get types */
+  const types = React.useMemo(() => {
+    return work.reduce((acc: string[] = [], cv: { type: string }) => {
+      if (!acc.includes(cv.type)) {
+        acc.push(unslugify(cv.type))
+      }
+
+      return acc
+    }, [])
+  }, [work])
+
+  console.log('t', types)
+
   return (
     <div className="p-12">
-      Greg Bridges
+      <h1 className={'text-3xl sm:text-6xl'}>Greg Bridges</h1>
+      <div className={'flex-col'}>
+        {types &&
+          types.map((type) => (
+            <button key={type} className={'flex text-3xl hover:text-rose-500 p-4'}>
+              {type}
+            </button>
+          ))}
+      </div>
     </div>
   )
 }

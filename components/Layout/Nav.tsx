01134b0234189dbd88c0349cb7b633ace86d47f6
Théo Mode <milesxbridges@gmail.com>
2022-07-02 18:39:05 -0700

    resolve errors

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -1,3 +1,4 @@
+// import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 const Nav = () => {
   return (
@@ -5,9 +6,18 @@ const Nav = () => {
       <div className={'flex flex-col justify-center items-center'}>
         <div className="text-2xl sm:text-6xl">Greg Bridges</div>
         <div className={"text-xs sm:text-normal pt-2"}>vibe creator circa 87</div>
+          {/*<div className={'flex h-32 w-32 rounded-full'}>*/}
+          {/*    <img src="https://arweave.net/ro4ciCRDxRPrtPX6GTl028KhBf1bAIoWVOZNOiGjwgQ" />*/}
+          {/*</div>*/}
       </div>
 
       <div id="connect" className="absolute right-4">
+        {/*<ConnectButton*/}
+        {/*  showBalance={true}*/}
+        {/*  label={'Connect'}*/}
+        {/*  chainStatus={'none'}*/}
+        {/*  accountStatus={'address'}*/}
+        {/*/>*/}
       </div>
     </div>
   )
